f203b9cde9dafdea1880efaee8878963056461b9
pawel <pawel.olejniczak@gmail.com>
2013-01-22 15:34:08 +0100

    fix height

--- widgets/disks/main.js
+++ widgets/disks/main.js
@@ -1,7 +1,7 @@
 (function(){
 	var w = $.extend({}, Widget);
 	
-	w.classes = 'doubleWidth autoHeight';
+	w.classes = 'doubleWidth';
 	
 	w.tableTpl = '<table><tr><th>disk</th><th>size</th><th>used</th><th>mount&nbsp;point</th>$1</tr></table>';
 	w.tableRowTpl = '<tr><td>$1</td><td class="tar">$2</td><td class="tar">$5</td><td>$6</td></tr>';
--- widgets/processes/main.js
+++ widgets/processes/main.js
@@ -1,7 +1,7 @@
 (function(){
 	var w = $.extend({}, Widget);
 	
-	w.classes = 'doubleWidth autoHeight'
+	w.classes = 'doubleWidth'
 	w.mainTpl = '<div class="processesData"></div>';
 	w.tableTpl = '<table><tr><th>user</th><th title="CPU usage">cpu</th><th title="Memory usage">mem</th><th>command</th></tr>$1</table>';
 	w.tableRowTpl = '<tr><td>$1</td><td class="tar">$3</td><td class="tar">$4</td>'+
